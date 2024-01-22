499cac7d48c622a2ee9902adbd1f3b7166e4aa2b
cantonerobin <cantonerobin97@gmail.com>
2024-01-22 19:39:30 +0100

    nginx config

---
+++ LXC_setup/setup.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Update System
+yum update kernel -y
+yum update -y && yum upgrade -y
+
+#System utils
+dnf install openssh-server nano sudo yum-utils wget curl zip iputils bind-utils traceroute -y
+
+#Docker install
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl enable docker
+systemctl start docker
+
+#Firewall
+yum install firewalld
+systemctl enable firewalld
+systemctl start firewalld
+
+firewall-cmd --zone=public --permanent --add-service=http
+firewall-cmd --zone=public --permanent --add-service=https
+firewall-cmd --zone=public --permanent --add-service=ssh
+firewall-cmd --reload
+
+#Cleanup
+dnf autoremove
